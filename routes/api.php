818d9430d56393dbcaf4f071a5dfbdf44c8f7b30
Rashod Chamikara <chamikara2008@gmail.com>
2026-06-13 12:55:51 +0530

    routes issue fix

--- routes/api.php
+++ routes/api.php
@@ -3,11 +3,11 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\WebsiteController;
 use App\Http\Controllers\ChatController;
+use App\Http\Middleware\ValidateEmbedToken;
 
 Route::middleware('auth')->post('/websites', [WebsiteController::class, 'store']);
 Route::middleware('embed.token')->post('/chat', [ChatController::class, 'message']);
-Route::middleware(['validate.embed'])->group(function () {
+Route::middleware([ValidateEmbedToken::class])->group(function () {
     Route::get('/widget/config', [ChatController::class, 'config']);
-    Route::post('/chat/message', [ChatController::class, 'message']);
     Route::post('/chat', [ChatController::class, 'message']);
 });
